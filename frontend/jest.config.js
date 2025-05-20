65f6770fa11a83293d7d66d23f030232da185f5c
vamsi <32238284+vkrish1707@users.noreply.github.com>
2025-05-20 11:15:04 -0400

    updated test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+    preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
-    },
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-      },
-      setupFilesAfterEnv: ['<rootDir>/setupTests.ts']
-  }
+      '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  };
